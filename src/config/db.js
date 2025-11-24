f717134b774b8f97a1634922d1d958630f1bae98
Mathias Mwaro <mattymatty9372@gmail.com>
2025-11-24 17:43:04 +0300

    Update MongoDB connection URI handling

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/gbv-support');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
