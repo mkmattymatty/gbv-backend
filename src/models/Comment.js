944c6b3007fad5b8cfeb1e3251f7e46adff72d2e
Mathias Mwaro <mattymatty9372@gmail.com>
2025-11-25 18:30:09 +0300

    Update Comment.js

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -5,11 +5,11 @@ const commentSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: false
+    required: false,
   },
-  username: { type: String, default: "Anonymous" },
+  username: { type: String, default: "Anonymous" }, // store logged-in username if sent
   text: { type: String, required: true, maxlength: 5000 },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
